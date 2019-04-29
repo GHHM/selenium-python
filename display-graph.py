e4d82534220d92bfed8abc28adaf75a781057e4e
Hyemi Kim <zozon1330@gmail.com>
2019-04-29 17:44:08 +0900

    add more graph type

--- display-graph.py
+++ display-graph.py
@@ -10,9 +10,12 @@ df = pd.read_csv(file_path,index_col=0)
 print(df)
 # df = pd.DataFrame(totalfreq)
 register_matplotlib_converters()
-plt.figure(figsize=(20,10))
+plt.figure(figsize=(15,10))
 plt.xticks(rotation=90)
 # plt.scatter(df.Date ,df.Frequency)
-plt.plot(df.Date,df.Frequency)
-plt.savefig('result.png')
+plt.bar(df.Date,df.Frequency) # 막대 그래프
+plt.scatter(df.Date,df.Frequency) # 산점도 그래프
+plt.plot(df.Date,df.Frequency, color='green') # 선그래프
+plt.title('Crawling Result') # 제목 추가
+plt.savefig('result.png') # 이미지로 저장
 plt.show()
