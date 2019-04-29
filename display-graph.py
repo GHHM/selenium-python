f97944156aca8b13fdcd5d09a2bb411072d4ee9a
Hyemi Kim <zozon1330@gmail.com>
2019-04-29 17:34:43 +0900

    Add save plot as image file

--- display-graph.py
+++ display-graph.py
@@ -5,6 +5,8 @@ import pandas as pd
 file_path = "./data.csv"
 
 df = pd.read_csv(file_path,index_col=0)
+
+
 print(df)
 # df = pd.DataFrame(totalfreq)
 register_matplotlib_converters()
@@ -12,4 +14,5 @@ plt.figure(figsize=(20,10))
 plt.xticks(rotation=90)
 # plt.scatter(df.Date ,df.Frequency)
 plt.plot(df.Date,df.Frequency)
+plt.savefig('result.png')
 plt.show()
