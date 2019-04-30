a67065854ace5f3bfeb5fd3270a0daa59e6a3021
Hyemi Kim <zozon1330@gmail.com>
2019-04-30 11:17:41 +0900

    compare two plot

--- display-graph.py
+++ display-graph.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
 import pandas as pd
 
-file_path = "./data.csv"
-
+file_path = "./yuhyundata.csv"
+file_path2 = "./data.csv"
 df = pd.read_csv(file_path,index_col=0)
-
+df2 = pd.read_csv(file_path2,index_col=0)
 
 print(df)
 # df = pd.DataFrame(totalfreq)
@@ -13,9 +13,14 @@ register_matplotlib_converters()
 plt.figure(figsize=(15,10))
 plt.xticks(rotation=90)
 # plt.scatter(df.Date ,df.Frequency)
-plt.bar(df.Date,df.Frequency) # 막대 그래프
-plt.scatter(df.Date,df.Frequency) # 산점도 그래프
+#plt.bar(df.Date,df.Frequency) # 막대 그래프
+
+# plt.scatter(df.Date,df.Frequency,color='blue') # 산점도 그래프
+# plt.scatter(df.Date,df2.Frequency,color='red')
+
 plt.plot(df.Date,df.Frequency, color='green') # 선그래프
+plt.plot(df.Date,df2.Frequency, color='red')
+
 plt.title('Crawling Result') # 제목 추가
 plt.savefig('result.png') # 이미지로 저장
 plt.show()
