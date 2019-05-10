3c63cec5cbd6532dbed0b8afacffc0d970fb3ac9
Hyemi Kim <zozon1330@gmail.com>
2019-05-10 16:12:36 +0900

    ntp time, get time diff, click by server time

--- automation_form_submit.py
+++ automation_form_submit.py
@@ -2,8 +2,20 @@ from selenium import webdriver as wd
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 import urllib.parse
-import datetime as dt
+import urllib.request
+from datetime import date, timedelta, datetime as dt
 import time
+
+def getServerTime(form_url):
+    serverDate = urllib.request.urlopen(form_url).headers['Date']
+    datetime_server = dt.strptime(serverDate, '%a, %d %b %Y %H:%M:%S %Z')
+    return datetime_server
+
+def getTimeDeff(form_url):
+    serverDate = urllib.request.urlopen(form_url).headers['Date']
+    datetime_server = dt.strptime(serverDate, '%a, %d %b %Y %H:%M:%S %Z')
+    datetime_local = dt.now()
+    return (datetime_local.second-datetime_server.second)
 
 # form.office.naver.com ip address
 # 210.89.164.67
@@ -33,17 +45,50 @@ btn2.click()
 btn3.click()
 btn4.click()
 
+# time_diff = getTimeDeff(form_url)
+# print(time_diff)
+
+# end = False
+# while not end : 
+#     tim = dt.now()
+#     if (tim.second+time_diff)>=59 and tim.microsecond>500000:
+#         btn_submit.click()
+#         end = True
+#         print('local time : ',tim)
+#         print('predicted server time : ', tim + timedelta(seconds = time_diff))
+#         print('submit complete')
+#     else:
+#         time.sleep(0.1)
+#         print(tim + timedelta(seconds = time_diff))
+
 end = False
-while not end : 
-    tim = dt.datetime.now()
-    if tim.second>=59 and tim.microsecond>500000:
+while not end :
+    tim = dt.now()
+    datetime_server = getServerTime(form_url)
+    if (datetime_server.second)>=59 and tim.microsecond>600000 :
         btn_submit.click()
         end = True
-        print(tim)
+        print('local time : ',tim)
+        print('predicted server time : ', datetime_server)
         print('submit complete')
     else:
         time.sleep(0.1)
-        print(tim)
+        print(datetime_server)
+
+
+# end = False
+# while not end : 
+#     tim = getServerTime(form_url)
+#     if(tim.second>=59 and tim.microsecond>500000):
+#          btn_submit.click()
+#          end = True
+#          print('local time : ',tim)
+#          print('predicted server time : ', tim + timedelta(seconds = time_diff))
+#          print('submit complete')
+#     else :
+#         time.sleep(0.1)
+#         print(tim)
+        
 
 
 #btn_submit = driver.find_elements_by_name('제출하기')
@@ -57,3 +102,6 @@ while not end :
 
 
 driver.kill()
+
+
+
--- server-time.py
+++ server-time.py
@@ -1,10 +1,27 @@
 import urllib.request
-import datetime as dt
+from datetime import date, timedelta, datetime as dt
+import time
  
-url = "https://blog.naver.com/musicaltalk/221533354110"
+url = "https://form.office.naver.com/form/responseView.cmd?formkey=MGNjNjIxN2UtZmJiYy00ZmM4LWFlNGUtMTVlMmQ3ZTNkMDVh&sourceId=urlshare"
 
-date = urllib.request.urlopen(url).headers['Date']
-print(date)
- 
-time = int(time.mktime(time.strptime(date, '%a, %d %b %Y %H:%M:%S %Z')))
-print(time)
+serverDate = urllib.request.urlopen(url).headers['Date']
+print('서버시간: ',serverDate)
+
+#타임스탬프 바꾸기
+#timestamp = (time.mktime(time.strptime(serverDate, '%a, %d %b %Y %H:%M:%S %Z')))
+
+datetime_server = dt.strptime(serverDate, '%a, %d %b %Y %H:%M:%S %Z')
+datetime_local = dt.now()
+
+print((datetime_local.second-datetime_server.second))
+
+while True:
+    serverDate = urllib.request.urlopen(url).headers['Date']
+    datetime_server = dt.strptime(serverDate, '%a, %d %b %Y %H:%M:%S %Z')
+    print('서버시간: ',datetime_server)
+    time.sleep(0.1)
+
+print('로컬시간: ',datetime_local)
+
+#print(type(date-serverDate), '초')
+
