5808b2423a1ba66e54159905d912f4773d1d9073
Hyemi Kim <zozon1330@gmail.com>
2019-05-29 16:51:57 +0900

    ajax crawling

--- beautiful_soup.py
+++ beautiful_soup.py
@@ -13,20 +13,29 @@ from IPython.display import Image
 # URl 소스 : https://image-comic.pstatic.net/webtoon/718020/33/20190507143410_c1fd5ebb0d38abd9b41d9aba3ac34e13_IMAG01_4.jpg
 # 귀곡의 문 : 33화 의문점
 
-image_url = 'https://image-comic.pstatic.net/webtoon/718020/33/20190507143410_c1fd5ebb0d38abd9b41d9aba3ac34e13_IMAG01_4.jpg'
+# image_url = 'https://image-comic.pstatic.net/webtoon/718020/33/20190507143410_c1fd5ebb0d38abd9b41d9aba3ac34e13_IMAG01_4.jpg'
 
-headers = {'Referer' : "https://comic.naver.com/webtoon/detail.nhn?titleId=718020&no=33&weekend=wed" }
+# headers = {'Referer' : "https://comic.naver.com/webtoon/detail.nhn?titleId=718020&no=33&weekend=wed" }
 
-response = requests.get(image_url, headers=headers)
-print(response) #200 = 성공
-image_data = response.content
-filename = os.path.basename(image_url)
+# response = requests.get(image_url, headers=headers)
+# print(response) #200 = 성공
+# image_data = response.content
+# filename = os.path.basename(image_url)
 
-with open(filename, 'wb') as f :
-    print("writing to {} {} bytes" .format(filename, len(image_data)))
-    f.write(image_data)
+# with open(filename, 'wb') as f :
+#     print("writing to {} {} bytes" .format(filename, len(image_data)))
+#     f.write(image_data)
 
 
 # #
-# 
+# Ajax 크롤링
 # #
+
+url = 'http://www.assembly.go.kr/assm/memact/congressman/memCond/memCondListAjax.do'
+params = {"currentPasge":1, "rowPerPage":100}
+res = requests.get(url, params=params)
+html = res.text
+soup = BeautifulSoup(html,'html.parser')
+soup.prettify
+print(soup.prettify)
+#tag.select_one['a'].text for tag in soup.select('dl')
