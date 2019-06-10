0f46b6a795d091438e9d515f876c1c972b38ebe2
Hyemi Kim <zozon1330@gmail.com>
2019-06-10 13:57:25 +0900

    backup

---
+++ autoCreateFormItems.py
@@ -0,0 +1,28 @@
+from selenium import webdriver as wd
+
+form_url = 'http://naver.me/G73wgGaT'
+
+# object 유형
+
+# 주관식 단답형
+# 주관식 서술형
+# 단일 선택형
+# 복수 선택형
+# 목록 선택형
+
+# 날짜/시간
+# 연락처
+# 주소
+# 금액/숫자
+# 이미지
+# 파일
+def CreateWebElement():
+    print('create web element')
+
+def ReadObject() :
+    print('read object')
+
+if __name__=='__main__':
+    # formItem 밑의 object 읽기
+    # Web element 생성
+    print('main')
